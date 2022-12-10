8985650891283940b82a43461136715a71ad77af
Hashemii2 <hashemi.faezeh2@yahoo.com>
2022-12-10 19:42:22 +0330

    memoize solution

--- dynamic/Knapsack.js
+++ dynamic/Knapsack.js
@@ -7,24 +7,24 @@ you can break the items.
 */
 
 //Solution1;
-// function Knapsack(values, weights, target) {
-//   let result = 0,
-//     totalWeight = 0,
-//     len = values.length;
+function Knapsack(values, weights, target) {
+  let result = 0,
+    totalWeight = 0,
+    len = values.length;
 
-//   for (let i = 0; i < len; i++) {
-//     const index = values.indexOf(Math.max(...values));
+  for (let i = 0; i < len; i++) {
+    const index = values.indexOf(Math.max(...values));
 
-//     if (totalWeight + weights[index] <= target) {
-//       result += values[index];
-//       totalWeight += weights[index];
-//     }
-//     values.splice(index, 1);
-//     weights.splice(index, 1);
-//   }
+    if (totalWeight + weights[index] <= target) {
+      result += values[index];
+      totalWeight += weights[index];
+    }
+    values.splice(index, 1);
+    weights.splice(index, 1);
+  }
 
-//   return result;
-// }
+  return result;
+}
 
 // let values = [10, 20, 30];
 // let weights = [1, 1, 1];
@@ -35,17 +35,17 @@ let weights = [10, 20, 30];
 let W = 50;
 
 //Slow Solution/recurtion
-// function slowKnapSack(W, weights, values, n = values.length) {
-//   if (n == 0 || W == 0) return 0;
-//   if (weights[n - 1] > W) return slowKnapSack(W, weights, values, n - 1);
-//   else
-//     return Math.max(
-//       values[n - 1] + slowKnapSack(W - weights[n - 1], weights, values, n - 1),
-//       slowKnapSack(W, weights, values, n - 1)
-//     );
-// }
+function slowKnapSack(W, weights, values, n = values.length) {
+  if (n == 0 || W == 0) return 0;
+  if (weights[n - 1] > W) return slowKnapSack(W, weights, values, n - 1);
+  else
+    return Math.max(
+      values[n - 1] + slowKnapSack(W - weights[n - 1], weights, values, n - 1),
+      slowKnapSack(W, weights, values, n - 1)
+    );
+}
 
-// console.log(slowKnapSack(W, weights, values));
+console.log(slowKnapSack(W, weights, values));
 
 function memoizeKnapsack(W, weights, values) {
   let cache = [];
